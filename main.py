b7dfec64e012b71846f3504ec9c81029a0397501
apsmi <apsmirnov2@gmail.com>
2013-10-15 17:27:57 +0400

    refactoring

--- main.py
+++ main.py
@@ -5,45 +5,39 @@
 import pygame
 from pygame import *
 from player import Player
-from blocks import Platform
+from blocks import Platform, PLATFORM_HEIGHT, PLATFORM_WIDTH
 from level import gen_level
 from bullet import Bullet
 from monster import Monster
 
-#Объявляем переменные
-WIN_WIDTH = 800 #Ширина создаваемого окна
-WIN_HEIGHT = 640 # Высота
-DISPLAY = (WIN_WIDTH, WIN_HEIGHT) # Группируем ширину и высоту в одну переменную
-BACKGROUND_COLOR = "#000000"
-
-PLATFORM_WIDTH = 32
-PLATFORM_HEIGHT = 32
-PLATFORM_COLOR = "#FF6262"
+def window_init(width, height, color, caption):
+    #инициализация окна
+    display = (width, height) # Группируем ширину и высоту в одну переменную
+    pygame.init() # Инициация PyGame, обязательная строчка
+    screen = pygame.display.set_mode(display) # Создаем окошко
+    pygame.display.set_caption(caption) # Пишем в шапку
+    bg = Surface((width,height)) # Создание видимой поверхности, будем использовать как фон
+    bg.fill(Color(color))     # Заливаем поверхность сплошным цветом
+    return bg, screen
 
 def main():
-    pygame.init() # Инициация PyGame, обязательная строчка 
-    screen = pygame.display.set_mode(DISPLAY) # Создаем окошко
-    pygame.display.set_caption("PyTanks") # Пишем в шапку
-    bg = Surface((WIN_WIDTH,WIN_HEIGHT)) # Создание видимой поверхности
-                                         # будем использовать как фон
-    bg.fill(Color(BACKGROUND_COLOR))     # Заливаем поверхность сплошным цветом
+    #инициализация
+    bg, screen = window_init(800, 640, "#000000", "PyTanks")
 
-    hero = Player(34,34) # создаем героя по (x,y) координатам
-    up = down = left = right = False    # по умолчанию — стоим
-
+    #группы объектов
     entities = pygame.sprite.Group() # Все объекты
-    monsters = pygame.sprite.Group() # Все передвигающиеся объекты
     platforms = [] # то, во что мы будем врезаться или опираться
 
-    entities.add(hero)
-    #platforms.append(hero)
+    #создаем героя
+    hero = Player(34,34) # создаем героя по (x,y) координатам
+    entities.add(hero) # добавляем героя во все объекты
+    up = down = left = right = False    # по умолчанию — стоим
 
     #2 координаты появления, скорость перемещения по горизонтали, скорость перемещения по вертикали, максимальное расстояние в одну сторону, которое может пройти монстр, по вертикали
     mn = Monster(736,580,1,1,20000,20000)
     entities.add(mn)
-    #platforms.append(mn)
-    monsters.add(mn)
 
+    #генерируем уровень
     level = gen_level (20,25)
 
     #рисуем платформы
@@ -58,50 +52,47 @@ def main():
         y += PLATFORM_HEIGHT    #то же самое и с высотой
         x = 0                   #на каждой новой строчке начинаем с нуля
 
+    #таймер
     timer = pygame.time.Clock()
 
-    isBullet = False
-    shutdirection = "up"
+    hero.isBullet = False
+    hero.shutdirection = "up"
 
     while 1: # Основной цикл программы
 
-        timer.tick(60)
+        timer.tick(60)#таймер на 60 кадров
 
         for e in pygame.event.get(): # Обрабатываем события
             #выход
             if e.type == QUIT:
                 raise SystemExit
 
+            #действия героя
             if e.type == KEYDOWN:
-
                 if e.key == K_LEFT:
-                    shutdirection = "left"
-
+                    hero.shutdirection = "left"
                     left = True
                     right = up = down = False
 
                 elif e.key == K_RIGHT:
-                    shutdirection = "right"
-
+                    hero.shutdirection = "right"
                     right = True
                     left = up = down = False
 
                 elif e.key == K_UP:
-                    shutdirection = "up"
-
+                    hero.shutdirection = "up"
                     up = True
                     left = right = down = False
 
                 elif e.key == K_DOWN:
-                    shutdirection = "down"
-
+                    hero.shutdirection = "down"
                     down = True
                     left = right = up = False
 
-                if e.key == K_SPACE and not isBullet:
-                    bullet = Bullet(hero.rect.left,hero.rect.top,shutdirection)
+                if e.key == K_SPACE and not hero.isBullet:
+                    bullet = Bullet(hero.rect.left,hero.rect.top,hero.shutdirection)
                     entities.add(bullet)
-                    isBullet = True
+                    hero.isBullet = True
 
             if e.type == KEYUP:
                 if e.key == K_RIGHT:
@@ -113,20 +104,21 @@ def main():
                 elif e.key == K_DOWN:
                     down = False
 
+        #выстрел монстра
         if mn.fire == 1 and not mn.isBullet:
             bullet_mn = Bullet(mn.rect.x,mn.rect.y,mn.shutdirection)
             entities.add(bullet_mn)
             mn.isBullet = True
 
-        screen.blit(bg, (0,0))      # Каждую итерацию необходимо всё перерисовывать
-
-        if isBullet:
-            bullet.update(platforms+ [mn])
+        #движение пули героя
+        if hero.isBullet:
+            bullet.update(platforms + [mn])
             if bullet.bum >= 20:
                 entities.remove(bullet)
                 bullet = None
-                isBullet = False
+                hero.isBullet = False
 
+        #движение пули монстра
         if mn.isBullet:
             bullet_mn.update(platforms + [hero])
             if bullet_mn.bum >= 20:
@@ -134,17 +126,20 @@ def main():
                 bullet_mn = None
                 mn.isBullet = False
 
+        #удаляем уничтоженные платформы
+        for p in platforms:
+            if (p.rect.width == 0) or (p.rect.height == 0):
+                platforms.remove(p)
+
         hero.update(left, right, up, down, platforms + [mn]) # передвижение
 
-        monsters.update(platforms + [hero] ) # передвигаем всех монстров
+        mn.update(platforms + [hero] ) # передвигаем всех монстров
+
+        screen.blit(bg, (0,0))      # Каждую итерацию необходимо всё перерисовывать
 
         entities.draw(screen) # отображение всего
 
         pygame.display.update()     # обновление и вывод всех изменений на экран
-
-        for p in platforms:
-            if (p.rect.width == 0) or (p.rect.height == 0):
-                platforms.remove(p)
 
 if __name__ == "__main__":
     main()
