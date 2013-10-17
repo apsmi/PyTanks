628fdcafbe3834d662388e00435f88229c5be1b1
Camzzz <temp15@mail.ru>
2013-10-17 11:55:04 +0400

    add monster_3 add life nps and hero

--- monster_config.py
+++ monster_config.py
@@ -18,8 +18,8 @@ class Monster_config_1 (Tank_config):
                           'tanks\m_a_down_2.png']
         self.INIT_IMAGE = "tanks\m_a_up_1.png"
 
-        self.maxLengthLeft = 150 # макс. пройденое расстояние от точки спавна
-        self.maxLengthUp = 150 # макс. пройденое расстояние от точки спавна
+        self.maxLengthLeft = 300 # макс. пройденое расстояние от точки спавна
+        self.maxLengthUp = 300 # макс. пройденое расстояние от точки спавна
 
         self.xvel = 1 # cкорость передвижения по горизонтали, 0 - стоит на месте
         self.yvel = 1 # скорость движения по вертикали, 0 - не двигается
@@ -49,3 +49,30 @@ class Monster_config_2 (Tank_config):
         self.yvel = 2 # скорость движения по вертикали, 0 - не двигается
 
         self.counter = 30 # счётчик поиска игрока (сложность)
+
+class Monster_config_3 (Tank_config):
+    def __init__(self, START_X, START_Y):
+        Tank_config.__init__(self)
+        self.START_X = START_X
+        self.START_Y = START_Y
+        self.ANIMATION_RIGHT = ['tanks\m_a_right_1.png',
+                           'tanks\m_a_right_2.png']
+        self.ANIMATION_LEFT = ['tanks\m_a_left_1.png',
+                          'tanks\m_a_left_2.png']
+
+        self.ANIMATION_UP = ['tanks\m_a_up_1.png',
+                        'tanks\m_a_up_2.png']
+        self.ANIMATION_DOWN = ['tanks\m_a_down_1.png',
+                          'tanks\m_a_down_2.png']
+        self.INIT_IMAGE = "tanks\m_a_up_1.png"
+
+        self.maxLengthLeft = 500 # макс. пройденое расстояние от точки спавна
+        self.maxLengthUp = 500 # макс. пройденое расстояние от точки спавна
+
+        self.xvel = 1 # cкорость передвижения по горизонтали, 0 - стоит на месте
+        self.yvel = 1 # скорость движения по вертикали, 0 - не двигается
+
+        self.counter = 30 # счётчик поиска игрока (сложность)
+
+        self.life = 3 # счётчик жизней
+        self.lifeStart = self.life
--- tank.py
+++ tank.py
@@ -12,6 +12,8 @@ class Tank_config():
         self.WIDTH = 28
         self.HEIGHT = 28
         self.COLOR =  "#888888"
+        self.life = 1 # начальные жизни игрока (сколько попаданий выдержит броня танка)
+        self.lifeStart = self.life
         self.ANIMATION_DELAY = 0.1 # скорость смены кадров
         self.ANIMATION_RIGHT = ['tanks\h_right_1.png',
                            'tanks\h_right_2.png']
@@ -79,5 +81,9 @@ class Tank(sprite.Sprite):
                     self.rect.top -= yvel # то не движется вверх
 
     def die(self):  #умираем, если попадает пуля
-        self.rect.x = self.startX
-        self.rect.y = self.startY
+        if self.config.life == 1:
+            self.rect.x = self.startX
+            self.rect.y = self.startY
+            self.config.life = self.config.lifeStart
+        else:
+            self.config.life -= 1
