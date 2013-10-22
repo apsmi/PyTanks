82cfab67f148bf61da4113d5dd0c2a9ac8aea325
apsmi <apsmirnov2@gmail.com>
2013-10-22 10:45:33 +0400

    monster now die correctly

--- monster.py
+++ monster.py
@@ -19,83 +19,97 @@ class Monster(Tank):
 
     def update(self, platforms, hero_y, hero_x): # по принципу героя
 
-        #наведение на героя
-
-        if self.config.counter == 0:
-            if 50 < random.randint (1,100) and hero_y != self.rect.y:
-                if self.impact == False:
-                    if hero_y > self.rect.y:
-                        self.course = 1
-                    elif hero_y < self.rect.y:
-                        self.course = 3
+        if (self.dead > 0):
+            if (self.dead < 30):
+                self.image = Surface((self.config.WIDTH,self.config.HEIGHT))
+                self.image.fill(Color(self.config.COLOR))
+                self.image.set_colorkey(Color(self.config.COLOR)) # делаем фон прозрачным
+                self.boltAnimDie.blit(self.image, (0, 0))  #animation
+                self.dead += 1
             else:
-                if self.impact == False:
+                self.rect.x = self.startX
+                self.rect.y = self.startY
+                self.dead = 0
+                self.image = image.load(self.config.INIT_IMAGE)
+        else:
+
+            #наведение на героя
+
+            if self.config.counter == 0:
+                if 50 < random.randint (1,100) and hero_y != self.rect.y:
+                    if self.impact == False:
+                        if hero_y > self.rect.y:
+                            self.course = 1
+                        elif hero_y < self.rect.y:
+                            self.course = 3
+                else:
+                    if self.impact == False:
+                        if hero_x > self.rect.x:
+                            self.course = 2
+                        elif hero_x < self.rect.x:
+                            self.course = 4
+                self.config.counter = 15
+
+
+            #проверка на макс. пройденое расстояние
+            if (abs(self.startX - self.rect.x) > self.config.maxLengthLeft):
+                self.course = 2
+                self.config.counter = 200
+                #self.config.xvel =-self.config.xvel  # если прошли максимальное растояние, то идеи в обратную сторону
+            if (abs(self.startY - self.rect.y) > self.config.maxLengthUp):
+                self.course = 1
+                self.config.counter = 200
+                #self.config.yvel = -self.config.yvel # если прошли максимальное растояние, то идеи в обратную сторону, вертикаль
+
+
+            #course - направление
+            #движение
+            if self.course == 1:
+                self.rect.y += self.config.yvel
+                self.image.fill(Color(self.config.COLOR))
+                self.boltAnimDown.blit(self.image, (0, 0))#animation
+                self.image = transform.rotate(self.image, 180)
+                self.shutdirection = "down"
+                self.collide(0, self.config.yvel, platforms) #проверяем столкновения
+            elif self.course == 2:
+                self.rect.x += self.config.xvel
+                self.image.fill(Color(self.config.COLOR))
+                self.boltAnimRight.blit(self.image, (0, 0))#animation
+                self.shutdirection = "right"
+                self.image = transform.rotate(self.image, 270)
+                self.collide(self.config.xvel, 0, platforms) #проверяем столкновения
+            elif self.course == 3:
+                self.rect.y -= self.config.yvel
+                self.image.fill(Color(self.config.COLOR))
+                self.boltAnimUp.blit(self.image, (0, 0))#animation
+                self.shutdirection = "up"
+                self.collide(0, -self.config.yvel, platforms) #проверяем столкновения
+            elif self.course == 4:
+                self.rect.x -= self.config.xvel
+                self.image.fill(Color(self.config.COLOR))
+                self.boltAnimLeft.blit(self.image, (0, 0))#animation
+                self.image = transform.rotate(self.image, 90)
+                self.shutdirection = "left"
+                self.collide(-self.config.xvel, 0, platforms) #проверяем столкновения
+
+
+            if self.impact == True:
+                if hero_y == self.rect.y:
                     if hero_x > self.rect.x:
                         self.course = 2
                     elif hero_x < self.rect.x:
                         self.course = 4
-            self.config.counter = 15
 
+                if hero_x == self.rect.x:
+                    if hero_y > self.rect.y:
+                        self.course = 1
+                    elif hero_y < self.rect.y:
+                        self.course = 3
 
-        #проверка на макс. пройденое расстояние
-        if (abs(self.startX - self.rect.x) > self.config.maxLengthLeft):
-            self.course = 2
-            self.config.counter = 200
-            #self.config.xvel =-self.config.xvel  # если прошли максимальное растояние, то идеи в обратную сторону
-        if (abs(self.startY - self.rect.y) > self.config.maxLengthUp):
-            self.course = 1
-            self.config.counter = 200
-            #self.config.yvel = -self.config.yvel # если прошли максимальное растояние, то идеи в обратную сторону, вертикаль
+            #стрельба
+            if self.isBullet == False :
+                if 40 > random.randint (1, 1000):
+                    self.fire = 1
 
-
-        #course - направление
-        #движение
-        if self.course == 1:
-            self.rect.y += self.config.yvel
-            self.image.fill(Color(self.config.COLOR))
-            self.boltAnimDown.blit(self.image, (0, 0))#animation
-            self.image = transform.rotate(self.image, 180)
-            self.shutdirection = "down"
-            self.collide(0, self.config.yvel, platforms) #проверяем столкновения
-        elif self.course == 2:
-            self.rect.x += self.config.xvel
-            self.image.fill(Color(self.config.COLOR))
-            self.boltAnimRight.blit(self.image, (0, 0))#animation
-            self.shutdirection = "right"
-            self.image = transform.rotate(self.image, 270)
-            self.collide(self.config.xvel, 0, platforms) #проверяем столкновения
-        elif self.course == 3:
-            self.rect.y -= self.config.yvel
-            self.image.fill(Color(self.config.COLOR))
-            self.boltAnimUp.blit(self.image, (0, 0))#animation
-            self.shutdirection = "up"
-            self.collide(0, -self.config.yvel, platforms) #проверяем столкновения
-        elif self.course == 4:
-            self.rect.x -= self.config.xvel
-            self.image.fill(Color(self.config.COLOR))
-            self.boltAnimLeft.blit(self.image, (0, 0))#animation
-            self.image = transform.rotate(self.image, 90)
-            self.shutdirection = "left"
-            self.collide(-self.config.xvel, 0, platforms) #проверяем столкновения
-
-
-        if self.impact == True:
-            if hero_y == self.rect.y:
-                if hero_x > self.rect.x:
-                    self.course = 2
-                elif hero_x < self.rect.x:
-                    self.course = 4
-
-            if hero_x == self.rect.x:
-                if hero_y > self.rect.y:
-                    self.course = 1
-                elif hero_y < self.rect.y:
-                    self.course = 3
-
-        #стрельба
-        if self.isBullet == False :
-            if 40 > random.randint (1, 1000):
-                self.fire = 1
-
-        self.config.counter -= 1 #уменьшаем счётчик цикла update
-        self.impact = False
+            self.config.counter -= 1 #уменьшаем счётчик цикла update
+            self.impact = False
